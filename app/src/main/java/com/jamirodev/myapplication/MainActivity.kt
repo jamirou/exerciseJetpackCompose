751c82d794e02e3ec665a7ac287bff6afc892fd0
Jamiro <jamirocristian15@gmail.com>
2023-07-17 18:44:38 -0400

    Ejercicio terminado...

--- app/src/main/java/com/jamirodev/myapplication/MainActivity.kt
+++ app/src/main/java/com/jamirodev/myapplication/MainActivity.kt
@@ -3,14 +3,24 @@ package com.jamirodev.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jamirodev.myapplication.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeArticle()
                 }
             }
         }
@@ -30,17 +40,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ComposeArticle() {
+     ArticleCompose (
+        title = stringResource(R.string.title_jetpack_compose_tutorial),
+        subtitle = stringResource(R.string.subtitle_jetpack_compose),
+        description = stringResource(R.string.description_jetpack_compose),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
     )
+}
+
+
+@Composable
+private fun ArticleCompose(
+    title: String,
+    subtitle: String,
+    description: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+    ){
+    Column(modifier = modifier) {
+        Image(painter = imagePainter,
+            contentDescription = null,
+            modifier = Modifier)
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = subtitle,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = description,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeArticlePreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        ComposeArticle()
     }
 }
